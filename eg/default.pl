9c66058fecf7ecdbd70a9cd15a59df562f4b7e77
xaicron <xaicron@gmail.com>
2009-04-06 00:50:55 +0900

    delete comment

--- eg/default.pl
+++ eg/default.pl
@@ -6,5 +6,3 @@ my $video_id = shift || die "Usage: $0 [video_id|video_url]";
 
 my $client = WWW::YouTube::Download->new(encode => 'cp932');
 $client->download($video_id);
-#my $url = $client->get_video_url('gVq85-rDlwk');
-#$client->user_agent->get($url, ':content_file' => $client->filename);
