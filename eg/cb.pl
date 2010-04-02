c90c219c8dc17de7f062ccf5149222032211f30c
xaicron <xaicron@gmail.com>
2010-04-03 02:55:17 +0900

    eg 書き換え

--- eg/cb.pl
+++ eg/cb.pl
@@ -4,17 +4,17 @@ use WWW::YouTube::Download;
 
 my $video_id = shift || die "Usage: $0 [video_id|video_url]";
 
-my $client = WWW::YouTube::Download->new(encode => 'cp932');
-$client->download($video_id, \&cb);
+my $client = WWW::YouTube::Download->new;
+$client->download($video_id, { cb => \&cb, fmt => 18 });
 
 my $fh;
 sub cb {
-	my ($data, $res, $proto) = @_;
-	
-	unless ($fh) {
-		open $fh, '>', $client->filename or die $client->filename, " $!";
-		binmode $fh;
-	}
-	
-	print $fh $data;
+    my ($data, $res, $proto) = @_;
+    
+    unless ($fh) {
+        open $fh, '>', "$video_id.mp4" or die "$video_id.mp4", " $!";
+        binmode $fh;
+    }
+    
+    print $fh $data;
 }
--- eg/default.pl
+++
@@ -1,8 +0,0 @@
-use strict;
-use warnings;
-use WWW::YouTube::Download;
-
-my $video_id = shift || die "Usage: $0 [video_id|video_url]";
-
-my $client = WWW::YouTube::Download->new(encode => 'cp932', verbose => 1);
-$client->download($video_id);
