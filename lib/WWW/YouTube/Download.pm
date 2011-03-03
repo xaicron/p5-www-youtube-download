c6e49dfae52ebf38a5fee684bbbe4953dda3ac7f
xaicron <xaicron@gmail.com>
2011-03-04 01:34:06 +0900

    fixed could not fetch json data problem (akiym++)

--- lib/WWW/YouTube/Download.pm
+++ lib/WWW/YouTube/Download.pm
@@ -167,7 +167,7 @@ sub _get_args {
 
     my $data;
     for (split "\n", $content) {
-        if ($_ && /videoplayback/ && /signature/ && !/HTML/) {
+        if ($_ && /var\s+swfConfig/ && /videoplayback/ && /signature/ && !/HTML/) {
             my ($json) = $_ =~ /^[^{]+(.*)[^}]+$/;
             $data = JSON->new->utf8(1)->decode($json);
             last;
