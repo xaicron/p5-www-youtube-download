68663488eb20580e87e6b2bbbf20b7d6ac6a5ff7
Mikolaj Kucharski <mikolaj@kucharski.name>
2013-05-16 19:00:54 +0100

    New youtube-playlists features: - new -n option to normalize given URLs, acts also as dry run - new -u option to parse given arguments as YouTube usernames and list their video uploads - Download.pm gets new method user_id() to parse URLs and return YouTube username - documentation updates - regress for above method - the tools still has max results limit of 50 items, for a given playlist or user uploads

--- lib/WWW/YouTube/Download.pm
+++ lib/WWW/YouTube/Download.pm
@@ -301,6 +301,15 @@ sub playlist_id {
     return $stuff;
 }
 
+sub user_id {
+    my ($self, $stuff) = @_;
+    return unless $stuff;
+    if ($stuff =~ m{/user/([^&#?=/;]+)}) {
+	return $1;
+    }
+    return $stuff;
+}
+
 1;
 __END__
 
@@ -479,6 +488,10 @@ Parses given URL and returns video ID.
 =item B<playlist_id($url)>
 
 Parses given URL and returns playlist ID.
+
+=item B<user_id($url)>
+
+Parses given URL and returns YouTube username.
 
 =item B<get_video_url($video_id)>
 
