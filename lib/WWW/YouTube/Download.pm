d741addc3416658f29f034415d170c1c7b61feab
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2019-10-06 03:04:45 +0100

    Pulled PR/10 by @ryochin.
    Original work by Ryo Okamoto <ryo@aquahill.net> and kindly rebased by
    Mohammad S Anwar <mohammad.anwar@yahoo.com>
    
    Co-authored-by: Ryo Okamoto <ryo@aquahill.net>

--- lib/WWW/YouTube/Download.pm
+++ lib/WWW/YouTube/Download.pm
@@ -75,6 +75,7 @@ sub download {
     $args->{cb} = $self->_default_cb({
         filename  => $filename,
         verbose   => $args->{verbose},
+        progress  => $args->{progress},
         overwrite => defined $args->{overwrite} ? $args->{overwrite} : 1,
     }) unless ref $args->{cb} eq 'CODE';
 
@@ -95,9 +96,24 @@ sub _is_supported_fmt {
     defined($data->{video_url_map}{$fmt}{url}) ? 1 : 0;
 }
 
+sub _progress {
+    my ($self, $args, $total) = @_;
+
+    if (not defined $args->{_progress}) {
+        eval "require Term::ProgressBar" or return;    ## no critic
+        $args->{_progress} = Term::ProgressBar->new( { count => $total, ETA => 'linear', remove => 0, fh => \*STDOUT } );
+        $args->{_progress}->minor( $total > 50_000_000 ? 1 : 0 );
+        $args->{_progress}->max_update_rate(1);
+
+        $args->{_progress}->message("Total $total");
+    }
+
+    return $args->{_progress};
+}
+
 sub _default_cb {
     my ($self, $args) = @_;
-    my ($file, $verbose, $overwrite) = @$args{qw/filename verbose overwrite/};
+    my ($file, $verbose, $overwrite, $progress) = @$args{qw/filename verbose overwrite progress/};
 
     croak "file exists! $file" if -f $file and !$overwrite;
     open my $wfh, '>', $file or croak $file, " $!";
@@ -111,6 +127,18 @@ sub _default_cb {
         if ($verbose || $self->{verbose}) {
             my $size = tell $wfh;
             my $total = $res->header('Content-Length');
+
+            if ($progress) {
+                if (my $p = $self->_progress($args, $total)){
+                    $p->update($size);
+                    return;
+                }
+                else{
+                    print "(You need Term::ProgressBar module to show progress bar with -P switch)\n";
+                    $progress = 0;
+                }
+            }
+
             printf "%d/%d (%.2f%%)\r", $size, $total, $size / $total * 100;
             print "\n" if $total == $size;
         }
